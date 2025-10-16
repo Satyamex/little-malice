ce3269a344e7b08a79f4bdcb33b67e5743d343d6
IgnCloudi <126395013+IgnCloudi@users.noreply.github.com>
2025-10-16 22:43:37 +0530

    Revert "The cursor and sprite not rotating fixed"
    This reverts commit 8aa8ef6cb9f6d71fc00bdeb8329ab5d6e5982603.

--- prefab_scripts/player.gd
+++ prefab_scripts/player.gd
@@ -4,11 +4,10 @@ extends Node2D
 @onready var player_gun_anchor: Node2D = $"."
 @onready var player_sprite: Sprite2D = $"../player_sprite"
 @onready var player_gun_sprite: Sprite2D = $player_gun_sprite
-@onready var player: CharacterBody2D = $".."
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	player_gun_anchor.look_at(get_global_mouse_position())
-	if get_global_mouse_position().x < player.global_position.x:
+	if get_global_mouse_position().x < self.position.x:
 		player_sprite.flip_h = true
 		player_gun_sprite.flip_v = true
 	else:
